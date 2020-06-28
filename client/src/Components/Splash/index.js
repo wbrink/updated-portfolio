8d0dded527a3b6650aa1db309330bb83f191b2b6
wbrink <will.brink55@gmail.com>
2020-06-28 00:45:16 -0700

    made styling for splash component mobile responsive and added backgroundcolor and padding to splash-links

--- client/src/Components/Splash/index.js
+++ client/src/Components/Splash/index.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import {HashLink as Link} from "react-router-hash-link";
 import "./style.css";
 
 function Splash(props) {
@@ -6,9 +7,14 @@ function Splash(props) {
     <div className="" style={{marginLeft: `${props.margin}px`}}>
       <div className="splash">
         <div className="overlay">
-          <h1 id="splash-title">Full Stack Web Developer</h1>
-          <div className="button">View Porfolio</div>
-          <div className="button">View Resume</div>
+          <h1 id="splash-title">Will Brink</h1>
+          <h4 id="splash-subtitle">Full Stack Web Developer</h4>
+          
+          <div className="splash-links-container">
+            <Link className="splash-link" smooth to="/#portfolio">View Portfolio</Link>
+            <a className="splash-link" href="">View Resume</a>
+          </div>
+          
 
         </div>
 
--- server.js
+++ server.js
@@ -5,16 +5,10 @@ const nodemailer = require("nodemailer");
 
 const PORT = process.env.PORT || 3001;
 
+// if in production use build folder as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client", "build")));
 }
-
-
-// require the api routes
-
-// app.get('/test', (req,res) => {
-//   res.send("hello world");
-// })
 
 
 // send all other requests to index.html so react takes over
