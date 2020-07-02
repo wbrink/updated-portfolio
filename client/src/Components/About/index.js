582969a10b8e66225af189e16b906474e8465d12
wbrink <will.brink55@gmail.com>
2020-07-01 22:15:01 -0700

    comment out about

--- client/src/Components/About/index.js
+++ client/src/Components/About/index.js
@@ -9,7 +9,7 @@ function About(props) {
   return (
     <div className="container" style={{marginLeft: `${props.margin}px`}} id="about" >
       <h1 className="title">About</h1>
-      <p>
+      {/* <p>
         Hi there! My name is will and I am currently enrolled in a coding bootcamp for web development. I am excited
         to try and gain the skills necessary to build websites both on the front end and backend. The little
         programming knowledge I have are a few pet projects with python so the opportunity to dive into popular web
@@ -19,7 +19,7 @@ function About(props) {
         When not programming my hobbies include golf and spending time with family. Working at a golf course made the
         sport very accessible for me. I played a lot growing up and was lucky enough to play two years in college. I
         enjoy spending time with family and love throwing a ball for my dog Buster at the park.
-      </p>
+      </p> */}
     </div>
   )
 }
